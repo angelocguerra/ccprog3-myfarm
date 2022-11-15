237ac7647e1294620d1a6459210ec3ebc5fe5a6c
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-11-16 02:27:16 +0800

    Added Farm class object and methods

--- Farm.java
+++ Farm.java
@@ -1,3 +1,11 @@
 public class Farm {
-    
+    private Tile tile = new Tile();
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
 }
