c1b69a14d218dac5467386f1a6650fb04e1e18ca
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-11-16 06:57:22 +0800

    Added Seed class constructor, attributes, and methods

--- Seed.java
+++ Seed.java
@@ -1,3 +1,21 @@
 public class Seed {
-    
+    private int amount;
+    private final Crop crop;
+
+    public Seed (Crop crop){
+        this.amount = 0;
+        this.crop = crop;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Crop getCrop() {
+        return crop;
+    }
 }
